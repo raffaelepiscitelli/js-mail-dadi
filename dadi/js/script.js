c61e49d8e9174ec15d58e6c62d0545c2a9b2542f
raffaelepiscitelli <raffaelepiscitelli1994@gmail.com>
2023-11-15 16:52:41 +0100

    Esercizio dadi completo

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,17 +1,17 @@
-const cpuRandomNumber = Math.floor(Math.random() * 6) + 1 + " CPU NUMBER";
-console.log(cpuRandomNumber);
+let cpuRandomNumber = Math.floor(Math.random() * 6) + 1;
+console.log(cpuRandomNumber + " CPU'S NUMBER");
 
-const playerRandomNumber = Math.floor(Math.random() * 6) + 1 + " PLAYER NUMBER";
-console.log(playerRandomNumber);
+let playerRandomNumber = Math.floor(Math.random() * 6) + 1;
+console.log(playerRandomNumber + " PLAYER'S NUMBER");
 
 if (cpuRandomNumber > playerRandomNumber) {
-    console.log("CPU WIN!")
+    console.log("CPU WIN!");
 }
 
 else if (cpuRandomNumber < playerRandomNumber) {
-    console.log("YOU WIN!")
+    console.log("YOU WIN!");
 }
 
 else if (cpuRandomNumber === playerRandomNumber) {
-    console.log("EVEN")
+    console.log("EVEN");
 }
